84b8b2bfbe995d3e61a4a12bb3e0fb7c7932ca47
Arthur Robert Munn <arthur.robert.munn@proton.me>
2025-02-25 18:19:16 -0500

    first commit for church

---
+++ prayers/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("details/<int:person_id>", views.details, name="details"),
+    path("update/<int:person_id>/", views.update, name="update"),
+    path("delete/<int:person_id>/", views.delete, name="delete"),
+    path("search/", views.search, name="search")
+]
